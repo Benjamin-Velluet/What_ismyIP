fc00eb42f6ed6b39c3215d28be726e3c3564ffbe
Benjamin Velluet <bvelluet@student.42.fr>
2017-10-13 23:45:48 +0200

    Il faut que je compare les ip

--- includes/what_ismyIP.h
+++ includes/what_ismyIP.h
@@ -6,13 +6,15 @@
 /*   By: bvelluet <bvelluet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/13 18:19:55 by bvelluet          #+#    #+#             */
-/*   Updated: 2017/10/13 22:48:34 by bvelluet         ###   ########.fr       */
+/*   Updated: 2017/10/13 23:44:41 by bvelluet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WHAT_ISMYID_H
 # define WHAT_ISMYID_H
 
+# include "../libft/include/ft_printf.h"
+# include "../libft/include/get_next_line.h"
 # include "../libft/include/libft.h"
 # include <stdlib.h>
 
--- srcs/main.c
+++ srcs/main.c
@@ -6,24 +6,21 @@
 /*   By: bvelluet <bvelluet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/13 18:22:20 by bvelluet          #+#    #+#             */
-/*   Updated: 2017/10/13 22:48:33 by bvelluet         ###   ########.fr       */
+/*   Updated: 2017/10/13 23:44:40 by bvelluet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/what_ismyIP.h"
 
-static char		*recupip(char *str)
+static char		*recupIP(char *str)
 {
 	int			c;
 	FILE		*sortie = popen("curl ifconfig.me", "r");
 	int			i;
 
 	i = 0;
-	if(sortie == NULL)
-	{
-		perror("Erreur popen ");
-		exit(EXIT_FAILURE);
-	}
+	if(!sortie)
+		ft_error("Unable to recover IP");
 	while ((c = fgetc(sortie)) != EOF)
 	{
 		str[i] = c;
@@ -33,12 +30,40 @@ static char		*recupip(char *str)
 	return (str);
 }
 
+static char		*recupid(char *command)
+{
+	int			fd;
+	char		*host;
+	char		*user;
+	char		*pass;
+
+	if ((fd = open("./users.id", O_RDONLY)) == -1)
+		ft_error("Missing users.id file");
+	if (!get_next_line(fd, &host))
+		ft_error("Missing host in users.id");
+	if (!get_next_line(fd, &user))
+		ft_error("Missing user in users.id");
+	if (!get_next_line(fd, &pass))
+		ft_error("Missing password in users.id");
+	command = ft_strnew(20 + ft_strlen(host) + ft_strlen(user) + ft_strlen(pass));
+	command = ft_strcat(command, "curl -u ");
+	command = ft_strcat(command, user);
+	command = ft_strcat(command, ":");
+	command = ft_strcat(command, pass);
+	command = ft_strcat(command, " -T ip.txt ");
+	command = ft_strcat(command, host);
+	return (command);
+}
+
 int				main(void)
 {
 	char	*ip;
+	char	*command;
 
 	ip = ft_memalloc(17);
-	ip = recupip(ip);
-	system("curl -u mendtechwj:password -T ip.txt ftp://ftp.cluster007.ovh.net:21/www/priver/ip/");
+	command = NULL;
+	ip = recupIP(ip);
+	command = recupid(command);
+	system(command);
 	return (0);
 }
